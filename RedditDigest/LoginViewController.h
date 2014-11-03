bc4d4b1a75e93528ace54d3389c86b3049efbdba
xeaza <taylorwrightsanson@gmail.com>
2014-11-03 14:36:00 -0600

    Adding Login screen and LoginViewController

---
+++ RedditDigest/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  RedditDigest
+//
+//  Created by Taylor Wright-Sanson on 11/3/14.
+//  Copyright (c) 2014 Richmond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
