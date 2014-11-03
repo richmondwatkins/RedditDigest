23254ba90905455bcea5148174e2bd1825f4c7e4
Argeiphontes <christopher.ruggles@aya.yale.edu>
2014-11-03 16:07:05 -0600

    Added Category ViewController File

---
+++ RedditDigest/CategoryViewController.m
@@ -0,0 +1,37 @@
+//
+//  CategoryViewController.m
+//  RedditDigest
+//
+//  Created by Christopher on 11/3/14.
+//  Copyright (c) 2014 Richmond. All rights reserved.
+//
+
+#import "CategoryViewController.h"
+
+@interface CategoryViewController ()
+
+@end
+
+@implementation CategoryViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
