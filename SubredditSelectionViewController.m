2a8139f67292edcd73c9b6fb8016c6b88119c144
Richmond Watkins <richmondwatkins@mac.com>
2014-11-04 01:55:26 -0600

    delete subreddit working

--- SubredditSelectionViewController.m
+++ SubredditSelectionViewController.m
@@ -81,7 +81,7 @@
     NSURLSessionDataTask* dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
         if (!error) {
             NSLog(@"%@",response);
-            //[self getter]; //THIS IS FOR TESTING THE SUBREDDIT GETTER METHOD
+            [self getter]; //THIS IS FOR TESTING THE SUBREDDIT GETTER METHOD
         }
     }];
     [dataTask resume];
@@ -114,12 +114,46 @@
                 NSLog(@"Links: %@", links);
                 [[RKClient sharedClient] upvote:links.firstObject completion:^(NSError *error) {
                     NSLog(@"Upvoted the link!");
+
+                    [self deleter:subreddit];
                 }];
             }];
         }
 
     }];
 
+    [dataTask resume];
+
+}
+
+-(void)deleter:(RKSubreddit *)subreddit{
+    NSMutableDictionary *tempDict = [[NSMutableDictionary alloc] initWithObjectsAndKeys:subreddit.name, @"name",subreddit.URL, @"url", nil];//testing puproses
+
+
+    NSUUID *deviceID = [UIDevice currentDevice].identifierForVendor;
+    NSString *deviceString = [NSString stringWithFormat:@"%@", deviceID];
+    NSString *urlString = [NSString stringWithFormat:@"http://192.168.1.4:3000/subreddits/delete/%@",  deviceString];
+
+    NSDictionary *objectToDelete = [[NSDictionary alloc] initWithObjectsAndKeys:tempDict, @"subreddit", nil];
+    NSError *error;
+    NSData *postData = [NSJSONSerialization dataWithJSONObject:objectToDelete options:0 error:&error];
+    NSURL *url = [[NSURL alloc] initWithString:[urlString stringByAddingPercentEscapesUsingEncoding:NSASCIIStringEncoding]];
+
+    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:url];
+    request.HTTPMethod = @"POST";
+
+    [request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [request setHTTPBody:postData];
+
+    NSURLSessionConfiguration* config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession* session = [NSURLSession sessionWithConfiguration:config];
+
+    NSURLSessionDataTask* dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (!error) {
+            NSLog(@"%@",response);
+            //[self getter]; //THIS IS FOR TESTING THE SUBREDDIT GETTER METHOD
+        }
+    }];
     [dataTask resume];
 
 }
