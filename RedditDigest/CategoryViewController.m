c511ffc12ca1f909fa6b533680887ed48326482a
Argeiphontes <christopher.ruggles@aya.yale.edu>
2014-11-03 19:59:57 -0600

    Collection View Cells with FlowLayout

--- RedditDigest/CategoryViewController.m
+++ RedditDigest/CategoryViewController.m
@@ -12,6 +12,8 @@
 @interface CategoryViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
 @property NSArray *categories;
 @property (strong, nonatomic) IBOutlet UICollectionView *collectionView;
+@property UICollectionViewFlowLayout *flowLayout;
+
 
 @end
 
@@ -20,6 +22,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.categories = [NSArray arrayWithObjects:@"fashion", @"beauty",@"health",@"US news",@"global news",@"politics",@"technology",@"film",@"science",@"humor",@"world explorer",@"books",@"business & finance",@"music",@"art & design",@"history",@"the future",@"surprise me!",@"offbeat",@"cooking",@"sports",@"geek",@"green",@"adventure", nil];
+
+    // Configure layout
+    self.collectionView.backgroundColor = [UIColor clearColor];
+    self.flowLayout = [[UICollectionViewFlowLayout alloc]init];
+    [self.flowLayout setItemSize:CGSizeMake(90, 100)];
+    [self.flowLayout setScrollDirection:UICollectionViewScrollDirectionVertical];
+    self.flowLayout.minimumInteritemSpacing = 0.0f;
+    self.flowLayout.minimumLineSpacing = 0.0f;
+    [self.collectionView setCollectionViewLayout:self.flowLayout];
+    self.collectionView.bounces = YES;
+    [self.collectionView setShowsHorizontalScrollIndicator:NO];
+    [self.collectionView setShowsVerticalScrollIndicator:NO];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -37,6 +51,7 @@
 -(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
     CategoryCustomCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:(@"CVCell") forIndexPath:indexPath];
+    [cell.categoryLabel sizeToFit];
     cell.categoryLabel.text = self.categories[indexPath.row];
     return cell;
 }
