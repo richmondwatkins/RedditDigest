a1dccc66224cb58c852478a7d38181754a81358e
Argeiphontes <christopher.ruggles@aya.yale.edu>
2014-11-06 14:34:35 -0600

    Added About Reddit VC

---
+++ RedditDigest/SettingsAboutRedditViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsAboutRedditViewController.h
+//  RedditDigest
+//
+//  Created by Christopher on 11/6/14.
+//  Copyright (c) 2014 Richmond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsAboutRedditViewController : UIViewController
+
+@end
