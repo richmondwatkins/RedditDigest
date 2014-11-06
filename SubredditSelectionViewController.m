1e1cec50aa16276294521947da81dc63a5ce0ef3
Richmond Watkins <richmondwatkins@mac.com>
2014-11-06 09:04:49 -0600

    now grabbing categories from server

--- SubredditSelectionViewController.m
+++ SubredditSelectionViewController.m
@@ -379,37 +379,6 @@
     }];
 }
 
--(void)deleter:(RKSubreddit *)subreddit
-{
-    NSMutableDictionary *tempDict = [[NSMutableDictionary alloc] initWithObjectsAndKeys:subreddit.name, @"name",subreddit.URL, @"url", nil];//testing puproses
-
-    NSUUID *deviceID = [UIDevice currentDevice].identifierForVendor;
-    NSString *deviceString = [NSString stringWithFormat:@"%@", deviceID];
-    NSString *urlString = [NSString stringWithFormat:@"http://192.168.1.4:3000/subreddits/delete/%@",  deviceString];
-
-    NSDictionary *objectToDelete = [[NSDictionary alloc] initWithObjectsAndKeys:tempDict, @"subreddit", nil];
-    NSError *error;
-    NSData *postData = [NSJSONSerialization dataWithJSONObject:objectToDelete options:0 error:&error];
-    NSURL *url = [[NSURL alloc] initWithString:[urlString stringByAddingPercentEscapesUsingEncoding:NSASCIIStringEncoding]];
-
-    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:url];
-    request.HTTPMethod = @"POST";
-
-    [request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-    [request setHTTPBody:postData];
-
-    NSURLSessionConfiguration* config = [NSURLSessionConfiguration defaultSessionConfiguration];
-    NSURLSession* session = [NSURLSession sessionWithConfiguration:config];
-
-    NSURLSessionDataTask* dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-        if (!error) {
-            NSLog(@"%@",response);
-            //[self getter]; //THIS IS FOR TESTING THE SUBREDDIT GETTER METHOD
-        }
-    }];
-    [dataTask resume];
-}
-
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
     DigestViewController *digestViewController = segue.destinationViewController;
     digestViewController.subredditsForFirstDigest = self.selectedSubreddits;
