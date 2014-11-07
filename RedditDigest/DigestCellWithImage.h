3814802bf81b69941a4fb69c354405303f895842
xeaza <taylorwrightsanson@gmail.com>
2014-11-07 00:09:11 -0600

    Adding Group for Digest View. Adding DigestCellWithImageTableViewCell

---
+++ RedditDigest/DigestCellWithImage.h
@@ -0,0 +1,13 @@
+//
+//  DigestCellWithImage.h
+//  RedditDigest
+//
+//  Created by Taylor Wright-Sanson on 11/7/14.
+//  Copyright (c) 2014 Richmond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DigestCellWithImage : UITableViewCell
+
+@end
---
+++ RedditDigest/DigestCellWithImage.m
@@ -0,0 +1,23 @@
+//
+//  DigestCellWithImage.m
+//  RedditDigest
+//
+//  Created by Taylor Wright-Sanson on 11/7/14.
+//  Copyright (c) 2014 Richmond. All rights reserved.
+//
+
+#import "DigestCellWithImage.h"
+
+@implementation DigestCellWithImage
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
