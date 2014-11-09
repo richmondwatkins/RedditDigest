2a9a26c407c45d415949b53c687d1b84fe522ee5
Richmond Watkins <richmondwatkins@mac.com>
2014-11-09 16:51:34 -0600

    subreddits being pulled from core data in selection view controller when navigated to from settings

--- Pods/RedditKit/Classes/Model/RKLink.h
+++ Pods/RedditKit/Classes/Model/RKLink.h
@@ -188,6 +188,8 @@
  */
 @property (nonatomic, copy, readonly) NSString *linkFlairText;
 
+//custom for RedditDigest..tells if the user is already subscribed when selecting new subreddits
+@property BOOL isCurrentlySubscribed;
 /**
  Whether or not the link has an image URL.
  */
