9c5b5bb1c2dc8ac43163ddec7bcf47ef05817774
xeaza <taylorwrightsanson@gmail.com>
2014-11-14 05:58:07 -0600

    Refactoring image view and web view pan gesture logic and functionality in to the wrapper VC, web view still needs constraint work

--- RedditDigest/ImagePostViewController.m
+++ RedditDigest/ImagePostViewController.m
@@ -6,28 +6,9 @@
 //  Copyright (c) 2014 Richmond. All rights reserved.
 //
 
-#define REDDIT_DARK_BLUE [UIColor colorWithRed:0.2 green:0.4 blue:0.6 alpha:1];
-
 #import "ImagePostViewController.h"
 
-@interface ImagePostViewController () <UIGestureRecognizerDelegate, UIScrollViewDelegate>
-
-@property (weak, nonatomic) IBOutlet UIView *statusBarBackground;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *verticalSpaceConstraint;
-
-@end
 @implementation ImagePostViewController
-
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-    if (!self.navController.navigationBarHidden) {
-        self.statusBarBackground.alpha = 0.0;
-    }
-    else {
-        self.statusBarBackground.alpha = 1.0;
-    }
-}
 
 -(void)viewDidAppear:(BOOL)animated
 {
@@ -43,100 +24,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    self.statusBarBackground.backgroundColor = REDDIT_DARK_BLUE;
 }
 
-- (IBAction)onPan:(UIPanGestureRecognizer *)panGesture
-{
-    typedef NS_ENUM(NSUInteger, UIPanGestureRecognizerDirection) {
-        UIPanGestureRecognizerDirectionUndefined,
-        UIPanGestureRecognizerDirectionUp,
-        UIPanGestureRecognizerDirectionDown,
-        UIPanGestureRecognizerDirectionLeft,
-        UIPanGestureRecognizerDirectionRight
-    };
-
-    static UIPanGestureRecognizerDirection direction = UIPanGestureRecognizerDirectionUndefined;
-
-    switch (panGesture.state) {
-        case UIGestureRecognizerStateBegan:
-        {
-            if (direction == UIPanGestureRecognizerDirectionUndefined) {
-
-                CGPoint velocity = [panGesture velocityInView:self.view];
-
-                BOOL isVerticalGesture = fabs(velocity.y) > fabs(velocity.x);
-
-                if (isVerticalGesture) {
-                    if (velocity.y > 0) {
-                        direction = UIPanGestureRecognizerDirectionUp;
-                    } else {
-                        direction = UIPanGestureRecognizerDirectionDown;
-                    }
-                }
-
-                else {
-                    if (velocity.x > 0) {
-                        direction = UIPanGestureRecognizerDirectionRight;
-                    } else {
-                        direction = UIPanGestureRecognizerDirectionLeft;
-                    }
-                }
-            }
-            if (direction == UIPanGestureRecognizerDirectionDown && !self.navController.navigationBarHidden) {
-                [self hideNavigationAndTabBars];
-            }
-            break;
-        }
-        case UIGestureRecognizerStateEnded:
-        {
-            if (direction == UIPanGestureRecognizerDirectionUp) {
-
-                if (self.navController.navigationBarHidden) {
-                    [self showNavigationAndTabBars];
-                }
-            }
-            direction = UIPanGestureRecognizerDirectionUndefined;
-            break;
-        }
-        default:
-            break;
-    }
-}
-
-- (void)hideNavigationAndTabBars
-{
-    [UIView animateWithDuration:0.2 animations:^{
-        self.statusBarBackground.alpha = 1.0;
-    }];
-
-    [UIView animateWithDuration:UINavigationControllerHideShowBarDuration animations:^{
-        [self.navController setNavigationBarHidden:YES animated:YES];
-        self.verticalSpaceConstraint.constant += self.navController.navigationBar.frame.size.height;
-        [self.view layoutIfNeeded];
-    }];
-}
-
-- (void)showNavigationAndTabBars
-{
-    [UIView animateWithDuration:0.2 animations:^{
-        self.statusBarBackground.alpha = 0.0;
-    }];
-    [UIView animateWithDuration:UINavigationControllerHideShowBarDuration animations:^{
-        [self.navController setNavigationBarHidden:NO animated:YES];
-        self.verticalSpaceConstraint.constant -= self.navController.navigationBar.frame.size.height;
-        [self.view layoutIfNeeded];
-    }];
-}
-
--(BOOL)prefersStatusBarHidden {
-    return YES;
-}
-
-// Allow simultaneous recognition
-- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
-{
-    return YES;
-}
 @end
