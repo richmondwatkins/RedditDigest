f6a4f07c2b09fc9d1199fdf756aaa1871d90afcc
xeaza <taylorwrightsanson@gmail.com>
2014-11-15 01:11:11 -0600

    Fixed constraints on webview detail post

--- RedditDigest/WebPostViewController.m
+++ RedditDigest/WebPostViewController.m
@@ -10,9 +10,10 @@
 
 #import "WebPostViewController.h"
 
-@interface WebPostViewController () //<UIGestureRecognizerDelegate, UIScrollViewDelegate>
+@interface WebPostViewController () <UIGestureRecognizerDelegate, UIScrollViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UIView *statusBarBackground;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *verticalSpaceConstraint;
 
 @end
 @implementation WebPostViewController
@@ -49,106 +50,104 @@
     }
 }
 
-//- (IBAction)onPan:(UIPanGestureRecognizer *)panGesture
-//{
-//    typedef NS_ENUM(NSUInteger, UIPanGestureRecognizerDirection) {
-//        UIPanGestureRecognizerDirectionUndefined,
-//        UIPanGestureRecognizerDirectionUp,
-//        UIPanGestureRecognizerDirectionDown,
-//        UIPanGestureRecognizerDirectionLeft,
-//        UIPanGestureRecognizerDirectionRight
-//    };
-//
-//    static UIPanGestureRecognizerDirection direction = UIPanGestureRecognizerDirectionUndefined;
-//
-//    switch (panGesture.state) {
-//        case UIGestureRecognizerStateBegan:
-//        {
-//            if (direction == UIPanGestureRecognizerDirectionUndefined) {
-//
-//                CGPoint velocity = [panGesture velocityInView:self.view];
-//
-//                BOOL isVerticalGesture = fabs(velocity.y) > fabs(velocity.x);
-//
-//                if (isVerticalGesture) {
-//                    if (velocity.y > 0) {
-//                        direction = UIPanGestureRecognizerDirectionUp;
-//                    } else {
-//                        direction = UIPanGestureRecognizerDirectionDown;
-//                    }
-//                }
-//
-//                else {
-//                    if (velocity.x > 0) {
-//                        direction = UIPanGestureRecognizerDirectionRight;
-//                    } else {
-//                        direction = UIPanGestureRecognizerDirectionLeft;
-//                    }
-//                }
-//            }
-//            if (direction == UIPanGestureRecognizerDirectionDown && !self.navigationController.navigationBarHidden) {
-//                [self hideNavigationAndTabBars];
-//            }
-//            break;
-//        }
-//        case UIGestureRecognizerStateEnded:
-//        {
-//            if (direction == UIPanGestureRecognizerDirectionUp) {
-//                if (self.navigationController.navigationBarHidden) {
-//                    [self showNavigationAndTabBars];
-//                }
-//            }
-//            direction = UIPanGestureRecognizerDirectionUndefined;
-//            break;
-//        }
-//        default:
-//            break;
-//    }
-//}
+#pragma mark - Pan
+- (IBAction)onPan:(UIPanGestureRecognizer *)panGesture
+{
+    typedef NS_ENUM(NSUInteger, UIPanGestureRecognizerDirection) {
+        UIPanGestureRecognizerDirectionUndefined,
+        UIPanGestureRecognizerDirectionUp,
+        UIPanGestureRecognizerDirectionDown,
+        UIPanGestureRecognizerDirectionLeft,
+        UIPanGestureRecognizerDirectionRight
+    };
 
-//- (void)hideNavigationAndTabBars
-//{
-//    // If visiable hide nav and tab bar when scroll begins.
-//    [UIView animateWithDuration:0.3 animations:^{
-//        [self.navigationController setNavigationBarHidden:YES animated:YES];
-//        self.tabBarController.tabBar.hidden = YES;
-//    }];
-//
-//    [UIView animateWithDuration:0.0 delay:0.1 options:UIViewAnimationOptionTransitionNone animations:^{
-//        self.statusBarBackground.alpha = 1.0;
-//    } completion:^(BOOL finished) {
-//        // Done
-//    }];
-//}
+    static UIPanGestureRecognizerDirection direction = UIPanGestureRecognizerDirectionUndefined;
 
-//- (void)showNavigationAndTabBars
-//{
-//    [UIView animateWithDuration:0.3 animations:^{
-//        [self.navigationController setNavigationBarHidden:NO animated:YES];
-//        self.tabBarController.tabBar.hidden = NO;
-//    }];
-//    // Animate after delay or there's a weird blip
-//    [UIView animateWithDuration:0.0 delay:0.1 options:UIViewAnimationOptionTransitionNone animations:^{
-//        self.statusBarBackground.alpha = 0.0;
-//    } completion:^(BOOL finished) {
-//        // Done
-//    }];
-//}
+    switch (panGesture.state) {
+        case UIGestureRecognizerStateBegan:
+        {
+            if (direction == UIPanGestureRecognizerDirectionUndefined) {
 
-//-(void)scrollViewDidScrollToTop:(UIScrollView *)scrollView
-//{
-//    [self showNavigationAndTabBars];
-//}
+                CGPoint velocity = [panGesture velocityInView:self.view];
 
-//-(BOOL)prefersStatusBarHidden {
-//    return YES;
-//}
-//
-//// Allow simultaneous recognition
-//- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
-//{
-//    return YES;
-//}
+                BOOL isVerticalGesture = fabs(velocity.y) > fabs(velocity.x);
+
+                if (isVerticalGesture) {
+                    if (velocity.y > 0) {
+                        direction = UIPanGestureRecognizerDirectionUp;
+                    } else {
+                        direction = UIPanGestureRecognizerDirectionDown;
+                    }
+                }
+
+                else {
+                    if (velocity.x > 0) {
+                        direction = UIPanGestureRecognizerDirectionRight;
+                    } else {
+                        direction = UIPanGestureRecognizerDirectionLeft;
+                    }
+                }
+            }
+            if (direction == UIPanGestureRecognizerDirectionDown && !self.navController.navigationBarHidden) {
+                [self hideNavigationAndTabBars];
+            }
+            break;
+        }
+        case UIGestureRecognizerStateEnded:
+        {
+            if (direction == UIPanGestureRecognizerDirectionUp) {
+
+                if (self.navController.navigationBarHidden) {
+                    [self showNavigationAndTabBars];
+                }
+            }
+            direction = UIPanGestureRecognizerDirectionUndefined;
+            break;
+        }
+        default:
+            break;
+    }
+}
+
+- (void)hideNavigationAndTabBars
+{
+    [UIView animateWithDuration:0.2 animations:^{
+        self.statusBarBackground.alpha = 1.0;
+    }];
+    NSLog(@"%f", self.verticalSpaceConstraint.constant);
+    [UIView animateWithDuration:UINavigationControllerHideShowBarDuration animations:^{
+        [self.navController setNavigationBarHidden:YES animated:YES];
+        self.verticalSpaceConstraint.constant = self.navController.navigationBar.frame.size.height -24;
+        [self.view layoutIfNeeded];
+    }];
+}
+
+- (void)showNavigationAndTabBars
+{
+    [UIView animateWithDuration:0.2 animations:^{
+        self.statusBarBackground.alpha = 0.0;
+    }];
+    [UIView animateWithDuration:UINavigationControllerHideShowBarDuration animations:^{
+        [self.navController setNavigationBarHidden:NO animated:YES];
+        self.verticalSpaceConstraint.constant = 0;
+        [self.view layoutIfNeeded];
+    }];
+}
+
+-(BOOL)prefersStatusBarHidden {
+    return YES;
+}
+
+// Allow simultaneous recognition
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
+{
+    return YES;
+}
+
+-(void)scrollViewDidScrollToTop:(UIScrollView *)scrollView
+{
+    [self showNavigationAndTabBars];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
