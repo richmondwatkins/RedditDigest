e314e9b873f473199de905f51ad65097322ad597
Richmond Watkins <richmondwatkins@mac.com>
2014-11-17 22:36:17 -0600

    storyboard setup for viewing past digests

---
+++ PastDigestsViewController.m
@@ -0,0 +1,24 @@
+//
+//  PastDigestsViewController.m
+//  RedditDigest
+//
+//  Created by Richmond on 11/17/14.
+//  Copyright (c) 2014 Richmond. All rights reserved.
+//
+
+#import "PastDigestsViewController.h"
+
+@interface PastDigestsViewController ()
+
+@end
+
+@implementation PastDigestsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+//DigestCell
+
+@end
