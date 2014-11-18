675fdee98e51423ff3c9d8ef60992a1ac4b786b0
Richmond Watkins <richmondwatkins@mac.com>
2014-11-18 14:31:01 -0600

    removing archived digests after 14

--- RedditDigest/Digest.m
+++ RedditDigest/Digest.m
@@ -17,6 +17,22 @@
 @dynamic digestPost;
 
 +(void)createDigestFromDigestPosts:(NSMutableArray *)digestPosts withManagedObject:(NSManagedObjectContext *)managedObject{
+    NSFetchRequest * fetch = [[NSFetchRequest alloc] init];
+    [fetch setEntity:[NSEntityDescription entityForName:@"Digest" inManagedObjectContext:managedObject]];
+    NSSortDescriptor *sorter = [[NSSortDescriptor alloc] initWithKey:@"time" ascending:YES];
+
+    [fetch setSortDescriptors:@[sorter]];
+    NSArray *digestArray = [managedObject executeFetchRequest:fetch error:nil];
+
+    if (digestArray.count >= 14) {
+        NSMutableArray *mutableDigests = [digestArray mutableCopy];
+        Digest *digestToDelete = [mutableDigests objectAtIndex:0];
+        [self clearOutContentsOfDigest:digestToDelete];
+        [managedObject deleteObject:digestToDelete];
+        [mutableDigests removeObjectAtIndex:0];
+        [managedObject save:nil];
+    }
+
     Digest *savedDigest = [NSEntityDescription insertNewObjectForEntityForName:@"Digest" inManagedObjectContext:managedObject];
     NSTimeInterval time = [[NSDate date] timeIntervalSince1970];
     savedDigest.time = [NSNumber numberWithDouble:time];
@@ -27,5 +43,44 @@
         [managedObject save:nil];
     }
 }
+
++(void)clearOutContentsOfDigest:(Digest *)digest{
+    NSArray *digestPosts = [digest.digestPost allObjects];
+
+    for (DigestPost *post in digestPosts) {
+        if ([post.image boolValue]) {
+            [self documentsPathForFileName:post.postID withPrefix:@"image-clone"];
+
+        }else if([post.thumbnailImagePath boolValue]){
+            [self documentsPathForFileName:post.postID withPrefix:@"thumbnail-clone"];
+
+        }else if([post.subredditImage boolValue]){
+            [self documentsPathForFileName:post.postID withPrefix:@"subreddit-clone"];
+
+        }
+    }
+}
+
+
++(void)documentsPathForFileName:(NSString *)postID withPrefix:(NSString *)prefix
+{
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory,NSUserDomainMask, YES);
+    NSString *documentsPath = [paths objectAtIndex:0];
+
+    NSString *pathCompenent = [NSString stringWithFormat:@"%@-%@",prefix, postID];
+    NSString *filePath = [documentsPath stringByAppendingPathComponent:pathCompenent];
+
+    [fileManager removeItemAtPath:filePath error:nil];
+}
+
+//NSFileManager *fileManager = [NSFileManager defaultManager];
+//NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+//
+//NSString *filePath = [documentsPath stringByAppendingPathComponent:[NSString stringWithFormat:@"image-%@", fileName]];
+//
+//NSError *error;
+//[fileManager removeItemAtPath:filePath error:&error];
 
 @end
