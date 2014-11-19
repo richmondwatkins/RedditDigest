84c998b8d729a41e5be0307b68e42dc8e708c1b1
Richmond Watkins <richmondwatkins@mac.com>
2014-11-19 11:47:41 -0600

    images displaying in archived digests

--- PastDigestsViewController.m
+++ PastDigestsViewController.m
@@ -9,10 +9,13 @@
 #import "PastDigestsViewController.h"
 #import "Digest.h"
 #import "DigestViewController.h"
+#import "DigestPost.h"
 @interface PastDigestsViewController () <UITableViewDataSource, UITableViewDelegate>
 @property (strong, nonatomic) IBOutlet UITableView *pastDigestTableView;
 @property NSArray *digests;
 @property NSArray *selectedDigestPosts;
+@property NSCache *imageCache;
+
 @end
 
 @implementation PastDigestsViewController
@@ -20,6 +23,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self retrievePastDigestFromCoreData];
+    self.imageCache = [[NSCache alloc] init];
 }
 
 
@@ -29,10 +33,9 @@
     [fetchDigests setSortDescriptors:@[sorter]];
 
     NSArray *digests = [self.managedObject executeFetchRequest:fetchDigests error:nil];
-    NSLog(@"Managed OBject %@", self.managedObject);
     if (digests) {
         self.digests = digests;
-        NSLog(@"Count %lu",(unsigned long)self.digests.count);
+
         [self.pastDigestTableView reloadData];
     }
 }
@@ -46,6 +49,19 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"DigestCell"];
     Digest *digest = [self.digests objectAtIndex:indexPath.row];
 
+    NSArray *posts = [digest.digestPost allObjects];
+    DigestPost *post = posts.firstObject;
+
+    if ([post.image boolValue]) {
+        cell.imageView.image = [self returnImageForCellFromData:post.postID withSubredditNameForKey:post.subreddit andFilePathPrefix:@"image-copy"];
+    }else if([post.thumbnailImagePath boolValue]){
+         cell.imageView.image = [self returnImageForCellFromData:post.postID withSubredditNameForKey:post.subreddit  andFilePathPrefix:@"thumbnail-copy"];
+    }else if([post.subredditImage boolValue]){
+         cell.imageView.image = [self returnImageForCellFromData:post.subreddit withSubredditNameForKey:post.subreddit andFilePathPrefix:@"subreddit-copy"];
+    }else{
+         cell.imageView.image = [UIImage imageNamed:@"snoo_camera_placeholder"];
+    }
+
     NSDate* date = [NSDate dateWithTimeIntervalSince1970:[digest.time doubleValue]];
 
     NSDateFormatter *dateFormat =[[NSDateFormatter alloc]init];
@@ -53,6 +69,25 @@
 
     cell.textLabel.text = [dateFormat stringFromDate:date];
     return cell;
+}
+
+-(UIImage *)returnImageForCellFromData:(NSString *)filePath withSubredditNameForKey:(NSString *)subreddit andFilePathPrefix:(NSString *)prefix{
+    UIImage *image = [self.imageCache objectForKey:subreddit];
+    if (image == nil) {
+        NSData *imageData = [NSData dataWithContentsOfFile:[self documentsPathForFileName:filePath withPrefix:prefix]];
+        image = [UIImage imageWithData:imageData];
+        [self.imageCache setObject:image forKey:subreddit];
+    }
+    return image;
+}
+
+- (NSString *)documentsPathForFileName:(NSString *)name withPrefix:(NSString *)prefix
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory,NSUserDomainMask, YES);
+    NSString *documentsPath = [paths objectAtIndex:0];
+
+    NSString *pathCompenent = [NSString stringWithFormat:@"%@-%@",prefix, name];
+    return [documentsPath stringByAppendingPathComponent:pathCompenent];
 }
 
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
