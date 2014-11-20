7f3dd16f08394ab1f913f53c93388ce12b180881
xeaza <taylorwrightsanson@gmail.com>
2014-11-19 22:50:37 -0600

    Adding pinch to zoom to image view

--- RedditDigest/ImagePostViewController.m
+++ RedditDigest/ImagePostViewController.m
@@ -2,8 +2,8 @@
 //  IndividualPostViewController.m
 //  RedditDigest
 //
-//  Created by Richmond on 11/7/14.
-//  Copyright (c) 2014 Richmond. All rights reserved.
+//  Created by Richmond, Taylor & Chris on 11/7/14.
+//  Copyright (c) 2014 Richmond, Taylor & Chris. All rights reserved.
 //
 
 #import "ImagePostViewController.h"
@@ -11,29 +11,12 @@
 @interface ImagePostViewController () <UIGestureRecognizerDelegate, UIScrollViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UIView *statusBarBackground;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *verticalSpaceConstraint;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
 
 @implementation ImagePostViewController
-
--(void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
-    [self.activityIndicator startAnimating];
-    self.activityIndicator.hidden = NO;
-    dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void){
-        UIImage *image = [UIImage imageWithData:[self documentsPathForFileName:self.postID]];
-        dispatch_async(dispatch_get_main_queue(), ^(void){
-            self.imageView.image = image;
-            self.imageView.contentMode = UIViewContentModeScaleAspectFit;
-            [self.activityIndicator stopAnimating];
-            self.activityIndicator.hidden = YES;
-        });
-    });
-}
 
 - (void)viewWillAppear:(BOOL)animated
 {
@@ -44,17 +27,124 @@
     else {
         self.statusBarBackground.alpha = 1.0;
     }
+}
 
-    self.scrollView.minimumZoomScale = 0.5;
-    self.scrollView.maximumZoomScale = 6.0;
-    self.scrollView.contentSize = CGSizeMake(1280, 960);
-    self.scrollView.delegate = self;
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self.activityIndicator startAnimating];
+    self.activityIndicator.hidden = NO;
+    dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void){
+        UIImage *image = [UIImage imageWithData:[self documentsPathForFileName:self.postID]];
+        dispatch_async(dispatch_get_main_queue(), ^(void){
+            // Only add view if it's not already there
+            if (![self.scrollView viewWithTag:1])
+            {
+                self.imageView = [[UIImageView alloc] initWithImage:image];
+                self.imageView.frame = (CGRect){.origin=CGPointMake(0.0f, 0.0f), .size=image.size};
+                self.imageView.tag = 1;
+                [self.scrollView addSubview:self.imageView];
+                self.scrollView.contentSize = image.size;
+
+                // Set up the minimum & maximum zoom scales
+                CGRect scrollViewFrame = self.scrollView.frame;
+                CGFloat scaleWidth = scrollViewFrame.size.width / self.scrollView.contentSize.width / 2;
+                CGFloat scaleHeight = scrollViewFrame.size.height / self.scrollView.contentSize.height / 2;
+                // Make min scale fill the screen width
+                CGFloat minScale = MIN(scaleWidth, scaleHeight) * 2;
+
+                self.scrollView.minimumZoomScale = minScale;
+                self.scrollView.maximumZoomScale = 1.0f;
+                self.scrollView.zoomScale = minScale;
+
+                [self centerScrollViewContents];
+            }
+
+            [self.activityIndicator stopAnimating];
+            self.activityIndicator.hidden = YES;
+        });
+    });
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    // Add a double tap gesture to zoom
+    UITapGestureRecognizer *doubleTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(scrollViewDoubleTapped:)];
+    doubleTapRecognizer.numberOfTapsRequired = 2;
+    doubleTapRecognizer.numberOfTouchesRequired = 1;
+    [self.scrollView addGestureRecognizer:doubleTapRecognizer];
 }
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+}
+
+#pragma mark - Scroll View Delegate
+
+- (void)centerScrollViewContents
+{
+    CGSize boundsSize = self.scrollView.bounds.size;
+    CGRect contentsFrame = self.imageView.frame;
+
+    if (contentsFrame.size.width < boundsSize.width) {
+        contentsFrame.origin.x = (boundsSize.width - contentsFrame.size.width) / 2.0f;
+    } else {
+        contentsFrame.origin.x = 0.0f;
+    }
+
+    if (contentsFrame.size.height < boundsSize.height) {
+        contentsFrame.origin.y = (boundsSize.height - contentsFrame.size.height) / 2.0f;
+    } else {
+        contentsFrame.origin.y = 0.0f;
+    }
+
+    self.imageView.frame = contentsFrame;
+}
+
+- (UIView*)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.imageView;
+}
+
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView
+{
+    // The scroll view has zoomed, so we need to re-center the contents
+    [self centerScrollViewContents];
+}
+
+#pragma mark - Tap and Double Tap Gestures 
+
+- (void)scrollViewDoubleTapped:(UITapGestureRecognizer*)recognizer
+{
+    CGPoint pointInView = [recognizer locationInView:self.imageView];
+
+    // Get a zoom scale that's zoomed in slightly, capped at the maximum zoom scale specified by the scroll view
+    CGFloat newZoomScale = self.scrollView.zoomScale * 1.5f;
+    newZoomScale = MIN(newZoomScale, self.scrollView.maximumZoomScale);
+
+    // Figure out the rect we want to zoom to, then zoom to it
+    CGSize scrollViewSize = self.scrollView.bounds.size;
+
+    CGFloat w = scrollViewSize.width / newZoomScale;
+    CGFloat h = scrollViewSize.height / newZoomScale;
+    CGFloat x = pointInView.x - (w / 2.0f);
+    CGFloat y = pointInView.y - (h / 2.0f);
+
+    CGRect rectToZoomTo = CGRectMake(x, y, w, h);
+
+    [self.scrollView zoomToRect:rectToZoomTo animated:YES];
+}
+
+// Allow simultaneous gesture recognition
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
+{
+    return YES;
+}
+
+#pragma mark - Documents Directory - Getting Images 
 
 - (NSData *)documentsPathForFileName:(NSString *)name
 {
@@ -72,116 +162,8 @@
     return [NSData dataWithContentsOfFile:filePath];
 }
 
-#pragma mark - Scroll View Delegate 
-
-- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
-{
-    return self.imageView;
-}
-
-
-#pragma mark - Pan
-- (IBAction)onPan:(UIPanGestureRecognizer *)panGesture
-{
-    typedef NS_ENUM(NSUInteger, UIPanGestureRecognizerDirection) {
-        UIPanGestureRecognizerDirectionUndefined,
-        UIPanGestureRecognizerDirectionUp,
-        UIPanGestureRecognizerDirectionDown,
-        UIPanGestureRecognizerDirectionLeft,
-        UIPanGestureRecognizerDirectionRight
-    };
-
-    static UIPanGestureRecognizerDirection direction = UIPanGestureRecognizerDirectionUndefined;
-
-    switch (panGesture.state) {
-        case UIGestureRecognizerStateBegan:
-        {
-            if (direction == UIPanGestureRecognizerDirectionUndefined) {
-
-                CGPoint velocity = [panGesture velocityInView:self.view];
-
-                BOOL isVerticalGesture = fabs(velocity.y) > fabs(velocity.x);
-
-                if (isVerticalGesture) {
-                    if (velocity.y > 0) {
-                        direction = UIPanGestureRecognizerDirectionUp;
-                    } else {
-                        direction = UIPanGestureRecognizerDirectionDown;
-                    }
-                }
-
-                else {
-                    if (velocity.x > 0) {
-                        direction = UIPanGestureRecognizerDirectionRight;
-                    } else {
-                        direction = UIPanGestureRecognizerDirectionLeft;
-                    }
-                }
-            }
-            if (direction == UIPanGestureRecognizerDirectionDown && !self.navController.navigationBarHidden) {
-                [self hideNavigationAndTabBars];
-            }
-            break;
-        }
-        case UIGestureRecognizerStateEnded:
-        {
-            if (direction == UIPanGestureRecognizerDirectionUp) {
-
-                if (self.navController.navigationBarHidden) {
-                    [self showNavigationAndTabBars];
-                }
-            }
-            direction = UIPanGestureRecognizerDirectionUndefined;
-            break;
-        }
-        default:
-            break;
-    }
-}
-
-- (void)hideNavigationAndTabBars
-{
-    [UIView animateWithDuration:0.2 animations:^{
-        self.statusBarBackground.alpha = 1.0;
-    }];
-    NSLog(@"%f", self.verticalSpaceConstraint.constant);
-    [UIView animateWithDuration:UINavigationControllerHideShowBarDuration animations:^{
-        [self.navController setNavigationBarHidden:YES animated:YES];
-        if (self.verticalSpaceConstraint.constant < 0) {
-            self.verticalSpaceConstraint.constant = 0;
-        }
-        else {
-            self.verticalSpaceConstraint.constant += self.navController.navigationBar.frame.size.height;
-        }
-        [self.view layoutIfNeeded];
-    }];
-}
-
-- (void)showNavigationAndTabBars
-{
-    [UIView animateWithDuration:0.2 animations:^{
-        self.statusBarBackground.alpha = 0.0;
-    }];
-    [UIView animateWithDuration:UINavigationControllerHideShowBarDuration animations:^{
-        [self.navController setNavigationBarHidden:NO animated:YES];
-        self.verticalSpaceConstraint.constant -= self.navController.navigationBar.frame.size.height;
-        [self.view layoutIfNeeded];
-    }];
-}
-
 -(BOOL)prefersStatusBarHidden {
     return YES;
-}
-
-// Allow simultaneous recognition
-- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
-{
-    return YES;
-}
-
--(void)scrollViewDidScrollToTop:(UIScrollView *)scrollView
-{
-    [self showNavigationAndTabBars];
 }
 
 @end
