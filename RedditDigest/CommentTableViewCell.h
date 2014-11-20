ba7f58afc948d5a32980b89aaae1b20a7fe2edf3
xeaza <taylorwrightsanson@gmail.com>
2014-11-20 05:53:16 -0600

    Adding comment vote score and author name to comments view

--- RedditDigest/CommentTableViewCell.h
+++ RedditDigest/CommentTableViewCell.h
@@ -15,6 +15,9 @@
 @property Comment *comment;
 @property (strong, nonatomic) IBOutlet UITextView *commentTextView;
 @property (weak, nonatomic) IBOutlet UILabel *hiddenLabelForCellSize;
+@property (weak, nonatomic) IBOutlet UILabel *userNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *voteScoreLabel;
+@property (weak, nonatomic) IBOutlet UIView *userDetailsBackgroundView;
 
 @end
 
