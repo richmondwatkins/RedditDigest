b51a306d0fe9875d6f298dd579b4deda7492f6c8
xeaza <taylorwrightsanson@gmail.com>
2014-11-28 16:24:43 -0500

    Sharing only shares the post url now.

--- RedditDigest/CommentViewController.m
+++ RedditDigest/CommentViewController.m
@@ -305,8 +305,8 @@
 #pragma mark - Share
 - (IBAction)onShareButtonPressed:(id)sender
 {
-    NSLog(@"SELECTED POST %@",self.self.post.url);
-    UIActivityViewController *activityViewController = [[UIActivityViewController alloc] initWithActivityItems:@[self.post.title, self.post.url]
+    NSLog(@"SELECTED POST %@", self.post.url);
+    UIActivityViewController *activityViewController = [[UIActivityViewController alloc] initWithActivityItems:@[self.post.url]
                                       applicationActivities:nil];
             [self.navigationController presentViewController:activityViewController
                                                    animated:YES
